b6ac6f7703a1f53c44e91c3598e4c5998e308bb8
Miska98 <124208420+Miska98@users.noreply.github.com>
2023-02-08 10:30:15 +0100

    HommeWork 1
    HomeWork1
    Java 11
    Java 17

---
+++ HomeWork.java
@@ -0,0 +1,9 @@
+package homework;
+
+public class HomeWork {
+
+    public static void main(String[] args) {
+        System.out.println("Homme Work Version Java 11");
+    }
+
+}
---
+++ HommeWork.java
@@ -0,0 +1,9 @@
+package work;
+
+public class HommeWork {
+
+    public static void main(String[] args) {
+        System.out.println("Homme Work Version Java 17");
+    }
+
+}

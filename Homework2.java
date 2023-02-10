67bc26a7f7f66cf905c751150857301358258f75
Miska98 <124208420+Miska98@users.noreply.github.com>
2023-02-10 22:07:17 +0100

    Hommework2
    Типы

---
+++ Homework2.java
@@ -0,0 +1,27 @@
+package homeworck2;
+
+public class Homework2 {
+    public static void main(String[] args) {
+
+
+
+
+        char a = 'G';
+        int b = 89;
+        byte c = 4;
+        short d = 56;
+        float e = 4.7_333_436f;
+        double f = 4.355_453_532;
+        long h = 12121L;
+
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(h);
+
+    }
+}
